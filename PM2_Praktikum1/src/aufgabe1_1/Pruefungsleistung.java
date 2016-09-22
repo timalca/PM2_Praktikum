a5c1a8ff0851f93735d3d9b818c2905346cc2d22
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2016-09-22 12:37:38 +0200

    Pruefungsleistung erstellt

---
+++ PM2_Praktikum1/src/aufgabe1_1/Pruefungsleistung.java
@@ -0,0 +1,12 @@
+/**
+ * PMT/PT1 Praktikum Aufgabenblatt 
+ * @author Johannes Kruber
+ * @author Luis Nickel 
+ */
+package aufgabe1_1;
+
+public class Pruefungsleistung
+{
+	String modulName;
+	int Note;	//1-15 Punkte
+}
