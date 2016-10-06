c6b3736e677d0ba313dfaea2fb883393bd85eec5
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2016-10-06 12:49:11 +0200

    init aufgabe 3

---
+++ PM2_Praktikum1/src/aufgabe1_3/ArrayListe.java
@@ -0,0 +1,17 @@
+/**
+ * PMT/PT1 Praktikum Aufgabenblatt 
+ * @author Johannes Kruber
+ * @author Luis Nickel 
+ */
+package aufgabe1_3;
+
+public class ArrayListe<T>
+{
+	int anzahlElemente;
+	private T[] elemente;
+	
+	public ArrayListe()
+	{
+		anzahlElemente = 0;
+	}
+}
