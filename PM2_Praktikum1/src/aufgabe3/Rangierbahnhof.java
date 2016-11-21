dac62bcf926b9219eba1e0d7a75e251eb6dd710f
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2016-11-21 10:29:30 +0100

    first solution 3.1

--- PM2_Praktikum1/src/aufgabe3/Rangierbahnhof.java
+++ PM2_Praktikum1/src/aufgabe3/Rangierbahnhof.java
@@ -5,7 +5,48 @@
  */
 package aufgabe3;
 
-public class Rangierbahnhof
-{
+import java.util.Observable;
 
+public class Rangierbahnhof extends Observable
+{
+	private Zug[] zuege;
+
+	public Rangierbahnhof(int anzahlGleise)
+	{
+		zuege = new Zug[anzahlGleise];
+	}
+
+	public void einfahren(Zug zug, int gleisnr)
+	{
+		aufZufahrtsGleis();
+		zuege[gleisnr] = zug;
+		setChanged();
+		notifyObservers();
+	}
+
+	public void ausfahren(int gleisnr)
+	{
+		aufZufahrtsGleis();
+		zuege[gleisnr] = null;
+		setChanged();
+		notifyObservers();
+	}
+
+	private synchronized void aufZufahrtsGleis()
+	{
+		try
+		{
+			Thread.sleep(2000);
+		}
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Zug getZug(int gleisnr)
+	{
+		return zuege[gleisnr];
+	}
 }
