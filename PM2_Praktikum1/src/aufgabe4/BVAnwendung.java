a7e082e04ee500a8a2dc3d786f95b428fed82637
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2016-12-05 10:21:10 +0100

    Einbinden von Codevorgaben

---
+++ PM2_Praktikum1/src/aufgabe4/BVAnwendung.java
@@ -0,0 +1,63 @@
+/**
+ * Prof. Philipp Jenke
+ * Hochschule für Angewandte Wissenschaften (HAW), Hamburg
+ * Lecture demo program.
+ */
+package aufgabe4;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import aufgabe4.braitenberg.braitenbergvehikel.BVBewegungAbstossung;
+import aufgabe4.braitenberg.braitenbergvehikel.BVBewegungAttraktion;
+import aufgabe4.braitenberg.braitenbergvehikel.BVSimulation;
+import aufgabe4.braitenberg.braitenbergvehikel.BraitenbergVehikel;
+import aufgabe4.braitenberg.braitenbergvehikel.Vektor2;
+import aufgabe4.braitenberg.view.BVCanvas;
+
+/**
+ * JavaFX Anwendung zur Darstellung und Interaktion mit einer
+ * Braitenberg-Vehikel-Simulation.
+ * 
+ * @author Philipp Jenke
+ */
+public class BVAnwendung extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    // Simulation zusammenstellen
+    BVSimulation sim = erzeugeSimulationsszene();
+
+    // Canvas setzen
+    BVCanvas canvas = new BVCanvas(600, 600, sim);
+
+    canvas.zeichneSimulation();
+
+    // Szenengraph aufbauen
+    primaryStage.setTitle("Braitenberg-Vehikel!");
+    BorderPane wurzel = new BorderPane();
+    wurzel.setCenter(canvas);
+
+    primaryStage.setScene(new Scene(wurzel, 850, 600));
+    primaryStage.show();
+  }
+
+  /**
+   * Erzeugt eine Simulationsszene zum Testen.
+   */
+  public BVSimulation erzeugeSimulationsszene() {
+    BraitenbergVehikel vehikel1 =
+        new BraitenbergVehikel("Susi", new BVBewegungAttraktion());
+    BraitenbergVehikel vehikel2 = new BraitenbergVehikel("Mike",
+        new BVBewegungAbstossung(), new Vektor2(-100, 100), new Vektor2(1, 0));
+    BVSimulation sim = new BVSimulation();
+    sim.vehikelHinzufuegen(vehikel1);
+    sim.vehikelHinzufuegen(vehikel2);
+    return sim;
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
