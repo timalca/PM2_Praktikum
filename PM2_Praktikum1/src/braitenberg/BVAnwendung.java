67b5cc934b7d76313fc069e18cbf7c6f34e63a6e
Luis Nickel <Luis Nickel@hotmail.de>
2016-12-13 17:51:38 +0100

    Vehikel Einstellung läuft

--- PM2_Praktikum1/src/braitenberg/BVAnwendung.java
+++ PM2_Praktikum1/src/braitenberg/BVAnwendung.java
@@ -12,10 +12,14 @@ import braitenberg.braitenbergvehikel.BraitenbergVehikel;
 import braitenberg.braitenbergvehikel.Vektor2;
 import braitenberg.view.BVCanvas;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -28,6 +32,26 @@ import javafx.stage.Stage;
  */
 public class BVAnwendung extends Application
 {
+	private class AddCombobox 
+	{
+		public AddCombobox(ComboBox<String> cBox,BraitenbergVehikel vehikel){
+			cBox.setOnAction(new EventHandler<ActionEvent>(){
+				
+				@Override
+				public void handle(ActionEvent arg0) {
+				
+					if(cBox.getValue()=="ABSTOSSUNG"){
+						vehikel.setBewegung(new BVBewegungAbstossung());
+					}
+					else{
+						vehikel.setBewegung(new BVBewegungAttraktion());
+					}
+				}
+				
+			});
+		}
+
+	};
 
 	@Override
 	public void start(Stage primaryStage)
@@ -48,10 +72,28 @@ public class BVAnwendung extends Application
 		checkbox.setText("Simuliere!");
 		Button button = new Button();
 		button.setText("Simuliere");
-		GridPane grid = new GridPane();
-		grid.add(button, 0, 0);
-		grid.add(checkbox, 0, 1);
-		wurzel.setLeft(grid);
+		GridPane simulationGrid = new GridPane();
+		GridPane bewegungsGrid =new GridPane();
+		
+		ObservableList<String> bewegungsList =FXCollections.observableArrayList("ATTRAKTION","ABSTOSSUNG");
+		for(int i=0;i<sim.getAnzahlVehike();i++){
+			ComboBox<String> bewegungCombobox=new ComboBox<String>(bewegungsList);
+			Label vehikelName=new Label(sim.getVehikel(i).getName());
+			bewegungsGrid.add(vehikelName,0,i);
+			bewegungsGrid.add(bewegungCombobox,1, i);
+			if(sim.getVehikel(i).getBewegung() instanceof BVBewegungAbstossung){
+				bewegungCombobox.setValue("ABSTOSSUNG");
+			}else{
+				bewegungCombobox.setValue("ATTRAKTION");
+			}
+			AddCombobox addCBox=new AddCombobox(bewegungCombobox, sim.getVehikel(i));
+		}
+		
+		simulationGrid.add(button, 0, 0);
+		simulationGrid.add(checkbox, 0, 1);
+		wurzel.setLeft(simulationGrid);
+		wurzel.setRight(bewegungsGrid);
+		
 
 		button.setOnAction(new EventHandler<ActionEvent>()
 		{
